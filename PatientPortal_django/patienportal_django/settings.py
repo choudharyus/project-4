9ba4a52186c4236c2f60c8ef7a9b73d45f95f7b3
Shahzad Choudhary <choudharyshahzada@johndeere.com>
2020-02-24 14:39:42 -0600

    new

--- PatientPortal_django/patienportal_django/settings.py
+++ PatientPortal_django/patienportal_django/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,8 +126,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# static files settings
 STATIC_URL = '/static/'
+# location where you will store your static files like bootstrap
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, "static"))]
+# location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Activate Django-Heroku
+import django_heroku
 django_heroku.settings(locals())
